2b99c07535810ab8df36ee83aed8d6181ad340a7
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-10 07:38:05 +0200

    add the javadocs repository (#2)

--- repositories.tf
+++ repositories.tf
@@ -14,3 +14,20 @@ resource "github_repository" "github-organization" {
   gitignore_template     = "Terraform"
   topics                 = ["terraform"]
 }
+
+resource "github_repository" "java-docs" {
+  name                   = "java-docs"
+  description            = "The java lecture notes represented as code. Everything as Code ❤️"
+  visibility             = "public"
+  has_issues             = true
+  has_projects           = false
+  has_wiki               = false
+  allow_merge_commit     = false
+  allow_squash_merge     = true
+  allow_rebase_merge     = false
+  allow_auto_merge       = true
+  delete_branch_on_merge = true
+  has_downloads          = false
+  gitignore_template     = "Node"
+  topics                 = ["docs", "java", "docusaurus"]
+}
