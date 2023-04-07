7e12b96ba86ab2e516e04c1a0ef59037c81caba4
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2023-04-07 11:13:24 +0200

    bump github provider (#8)
    * bump github provider
    
    * update gh actions
    
    * bump terraform version

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 4.0"
+      version = "~> 5.20.0"
     }
   }
   cloud {
@@ -11,6 +11,8 @@ terraform {
       name = "jappuccini-github-org"
     }
   }
+
+  required_version = ">= 1.4.4, < 1.5.0"
 }
 
 provider "github" {
